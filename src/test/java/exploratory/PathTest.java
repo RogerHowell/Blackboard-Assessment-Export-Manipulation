69f8a3bbfbba4879091cb01ebdb29d14ad9c9e15
Roger Howell <howelrtc@aston.ac.uk>
2019-11-13 23:37:24 +0000

    added exploratory test to examine if path separator is cross-system compatible when seeking the test resources directory (note all these tests pass locally on Win7 x64 JDK 11 machine -- may not all pass on ubuntu github actions system) #11

--- src/test/java/exploratory/PathTest.java
+++ src/test/java/exploratory/PathTest.java
@@ -3,8 +3,15 @@ package exploratory;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+/**
+ * Exploratory tests examining whether the direction of slashes is relevant for getting paths across systems.
+ */
 public class PathTest {
 
 
@@ -12,33 +19,68 @@ public class PathTest {
     public void printRootPath() {
 
         System.out.println("File.separator = " + File.separator);
-
         System.out.println("Paths.get(\"./\") = " + Paths.get("./"));
-
         System.out.println("Paths.get(\"/\") = " + Paths.get("/"));
-
         System.out.println("Paths.get(\"\\\\\") = " + Paths.get("\\"));
-
         System.out.println("Paths.get(\".\\\\\") = " + Paths.get(".\\"));
-
         System.out.println("Paths.get(File.separator) = " + Paths.get(File.separator));
-
         System.out.println("Paths.get(\".\" + File.separator) = " + Paths.get("." + File.separator));
 
         System.out.println();
         System.out.println("== .toAbsolutePath() ==");
-
         System.out.println("Paths.get(\"./\") = " + Paths.get("./").toAbsolutePath());
-
         System.out.println("Paths.get(\"/\") = " + Paths.get("/").toAbsolutePath());
-
         System.out.println("Paths.get(\"\\\\\") = " + Paths.get("\\").toAbsolutePath());
-
         System.out.println("Paths.get(\".\\\\\") = " + Paths.get(".\\").toAbsolutePath());
-
         System.out.println("Paths.get(File.separator) = " + Paths.get(File.separator).toAbsolutePath());
-
         System.out.println("Paths.get(\".\" + File.separator) = " + Paths.get("." + File.separator).toAbsolutePath());
+
+    }
+
+
+    @Test
+    public void testResourcesPathTest() {
+
+        Path path;
+
+        System.out.println();
+        System.out.println("== Forward slashes ==");
+        path = Paths.get("src/test/resources");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+        path = Paths.get("src/test/resources/");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+        path = Paths.get("./src/test/resources");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+        path = Paths.get("./src/test/resources/");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+
+        System.out.println();
+        System.out.println("== Backslashes ==");
+
+        path = Paths.get("src\\test\\resources");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+        path = Paths.get("src\\test\\resources\\");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+        path = Paths.get(".\\src\\test\\resources");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
+        path = Paths.get(".\\src\\test\\resources\\");
+        System.out.println("path.toFile().exists() :: " + path.toFile().exists());
+        assertTrue(path.toFile().exists());
+
 
     }
 
