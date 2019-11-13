0401aa2c428cf4d722c15808d20d2da95e266d2b
Roger Howell <howelrtc@aston.ac.uk>
2019-11-13 21:29:16 +0000

    tests re: optionally doing file existence check #7 #8

--- src/test/java/io/github/rogerhowell/model/BbExportZipTest.java
+++ src/test/java/io/github/rogerhowell/model/BbExportZipTest.java
@@ -6,7 +6,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BbExportZipTest {
 
@@ -24,5 +26,39 @@ public class BbExportZipTest {
     public void constructorTest_null() {
         final BbExportZip bbExport = new BbExportZip(null);
         assertNull(bbExport.getPath());
+    }
+
+
+    @Test
+    public void constructorTest_verifyFileExists_existingFile() {
+        final Path path = Paths.get("empty_zip/gradebook_2019_CS9999_Empty20Task_2019-11-08-21-41-57.zip");
+
+        boolean exists = true;
+
+        try {
+            final boolean     verifyFileExists = true;
+            final BbExportZip bbExport         = new BbExportZip(path, verifyFileExists);
+        } catch (final IllegalArgumentException e) {
+            exists = false;
+        }
+
+        assertTrue(exists, "File should exist.");
+    }
+
+
+    @Test
+    public void constructorTest_verifyFileExists_nonExistentFile() {
+        final Path path = Paths.get("/non-existent.zip");
+
+        boolean exists = true;
+
+        try {
+            final boolean     verifyFileExists = true;
+            final BbExportZip bbExport         = new BbExportZip(path, verifyFileExists);
+        } catch (final IllegalArgumentException e) {
+            exists = false;
+        }
+
+        assertFalse(exists, "File should not exist!!");
     }
 }
